45082fe10e75729000ecbc393e2be812dc2e02a6
Anandhaprakashs <harianandhpa@gmail.com>
2017-06-08 17:48:09 +0530

    Create GFG.java

---
+++ GFG.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+class GFG 
+{
+	public static void main (String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		char a[]={'a','e','i','o','u'};
+		String str=s.next();
+		char[] c=str.toCharArray();
+		int d=0;
+		for(int i=0;i<5;i++)
+		{
+		    if(c[0]==a[i])
+		    {
+		        System.out.println("Vowels");
+		        d=1;
+		    }
+		}
+		if(d==0)
+		System.out.println("Consonent");
+	}
+}
